83ded9ace0e1986737d3521d76fee656921e50b3
Agrim Shukla <andhakaaramay@gmail.com>
2025-05-11 22:40:03 +0530

    class 1 and class 2 compleate 11/05/2025 22:39

--- hello.c++
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Hello World it is my first program in c++ "<<endl;
-    return 0;
-}
---
+++ hello.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+// int main(){
+//     cout<<"Hello World"<<endl;
+//     return 0;
+// }
+
+// int main(){
+//     cout<<"enter your name: "<<endl;
+//     string name;
+//     cin>>name;
+//     cout<<"hello "<<name<<endl;
+//     return 0;
+// }
+
+// int main(){
+// int a=5;
+// int b=2;
+// float c = (float) a/b ;
+// cout<<"division is "<<c<<endl;
+// return 0;
+// }
+
+int main(){
+    cout<<"enter your number:"<<endl;
+    int a;
+    cin>>a;
+    cout <<"enter your secend number:"<<endl;
+    int b;
+    cin>>b;
+    cout<<"this is your sum number "<<a+b<<endl;
+
+}
